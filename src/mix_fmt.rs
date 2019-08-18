2f80ca27c119d4617e8e17e28b13219c57a91b7c
Alvin Kerber <alvin@alvinkerber.net>
2019-08-18 09:58:36 -0700

    off by one bug fix

--- src/mix_fmt.rs
+++ src/mix_fmt.rs
@@ -8,7 +8,7 @@ pub fn fmt_words(in_bytes: &Vec<u8>) -> String {
     let mut ix: usize = 0;
     let mut fmt_str = String::new();
     while ix + 3 < in_bytes.len() {
-        let w = word_from(&in_bytes[ix..ix+3]);
+        let w = word_from(&in_bytes[ix..ix+4]);
         fmt_str.push_str(&fmt_word(w));
         fmt_str.push('\n');
         ix += 4;
